62c7d8edc272edabdd5771bc5b22eb1cb29da949
bcpletcher <pletcher.accounts@icloud.com>
2025-07-27 20:08:10 -0400

    Updated env config file

--- frontend/env-config.mjs
+++ frontend/env-config.mjs
@@ -1,6 +1,11 @@
-import { config } from "dotenv";
 import { resolve } from "path";
 
-const environment = process.env.NODE_ENV || "development";
-const envFile = `.env.${environment}`;
-config({ path: resolve(process.cwd(), envFile) });
+(async () => {
+  if (process.env.NETLIFY !== "true") {
+    // Only load .env files locally, not on Netlify
+    const { config } = await import("dotenv");
+    const environment = process.env.NODE_ENV || "development";
+    const envFile = `.env.${environment}`;
+    config({ path: resolve(process.cwd(), envFile) });
+  }
+})();
